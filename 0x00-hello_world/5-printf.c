caa01ebd8dd74db7104cdc369753395d7ff54973
kitech <emmakagbabuwe@gmail.com>
2022-09-12 14:30:52 -0700

    Made used of Printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (o)
+}
+
+	
