0d4736f56f43d95ee6dae28c84b89017f59a3d2f
kitech <emmakagbabuwe@gmail.com>
2022-09-23 01:53:37 -0700

    Bunch of c commands

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,14 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_alphabet - prints alphabet
- *
- * Return: always 0;
- *
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
 void print_alphabet(void)
 {
-	char i;
+char letter;
 
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+
+_putchar('\n');
 }
